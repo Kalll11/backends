aa1d7939961552965773fd5960a62c9e23d00259
Cikal Arifin <cikal.123140109@student.itera.ac.id>
2025-11-18 19:20:39 +0700

    Delete models directory

--- models/kritiksaran.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const KritikSaranSchema = new mongoose.Schema({
-  nama: { type: String, required: true },
-  pesan: { type: String, required: true },
-  tanggal: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("KritikSaran", KritikSaranSchema);
